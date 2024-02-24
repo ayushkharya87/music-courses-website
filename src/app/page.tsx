88c8fa0b9e1a5ec87bfae6dd3b062a893dda00e8
ayush <ayush87269@gmail.com>
2024-02-24 13:31:59 +0530

    Description of your changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ import { Button } from './ui/moving-border'
 
 const HeroSection = () => {
   return (
-    <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 text-white'>
+    <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 '>
 
       {/* spotlight */}
       <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
