a336a26e08dd4006decac3e5c39d7bbdafc381d3
ayush <ayush87269@gmail.com>
2024-02-29 00:15:36 +0530

    final changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MovingCards from "@/components/MovingCards";
@@ -14,6 +15,7 @@ export default function Home() {
       <MovingCards />
       <UpcomingWebinars />
       <Instructors />
+      <Footer />
     </main>
   );
 }
